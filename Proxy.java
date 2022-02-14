b22ae7604b0e826ab544d300127c2c5cdf346487
李紫轩 <zli3@andrew.cmu.edu>
2022-02-14 03:45:36 -0500

    Open function prototype

--- Proxy.java
+++ Proxy.java
@@ -1,12 +1,63 @@
 /* Sample skeleton for proxy */
 
 import java.io.*;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Proxy {
 
-    private static class FileHandler implements FileHandling {
+    private static Integer fd = 9;
+    private static Map<Integer, String> fdPath = new ConcurrentHashMap<Integer, String>();
+    private static Map<Integer, RandomAccessFile> fdRAF = new ConcurrentHashMap<Integer, RandomAccessFile>();
 
+    private static class FileHandler implements FileHandling {
+        /**
+         * Handle open() request from client
+         * @param path path of target file
+         * @param o open option
+         * @return file descriptor or -errno
+         */
         public int open( String path, OpenOption o ) {
+            Integer currFd;
+            path = normalize(path);
+            File file = new File(path);
+            if (!file.exists() && (o == OpenOption.READ || o == OpenOption.WRITE)) {
+                return Errors.ENOENT;
+            } else if (file.isDirectory()) {
+                // May be wrong condition
+                return Errors.EISDIR;
+            }
+            try {
+                switch (o) {
+                    case READ:
+                        currFd = fetchFd();
+                        fdPath.put(currFd, path);
+                        fdRAF.put(currFd, new RandomAccessFile(file, "r"));
+                        break;
+                    case WRITE:
+                        currFd = fetchFd();
+                        fdPath.put(currFd, path);
+                        fdRAF.put(currFd, new RandomAccessFile(file, "w"));
+                        break;
+                    case CREATE:
+                        currFd = fetchFd();
+                        fdPath.put(currFd, path);
+                        fdRAF.put(currFd, new RandomAccessFile(file, "rw"));
+                        break;
+                    case CREATE_NEW:
+                        if (file.exists()) return Errors.EEXIST;
+                        currFd = fetchFd();
+                        fdPath.put(currFd, path);
+                        fdRAF.put(currFd, new RandomAccessFile(file, "rw"));
+                        break;
+                    default:
+                        return Errors.EINVAL;
+                }
+            } catch (FileNotFoundException e) {
+                return Errors.ENOENT;
+            }
             return Errors.ENOSYS;
         }
 
@@ -32,6 +83,19 @@ public class Proxy {
 
         public void clientdone() {
             return;
+        }
+
+        /**
+         * Normalize possible redundant path.
+         * @param OrigPath Original path
+         * @return Normalized absolute path
+         */
+        private String normalize(String OrigPath) {
+            return FileSystems.getDefault().getPath(OrigPath).normalize().toString();
+        }
+
+        private Integer fetchFd() {
+            return fd++;
         }
 
     }
