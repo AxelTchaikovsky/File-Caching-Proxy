59ab62af33eebc9b4e1e0f18b66133d52608d4b7
李紫轩 <zli3@andrew.cmu.edu>
2022-03-04 00:13:30 -0500

    Origanize shell scripts

--- test_scipts/slowReadA.sh
+++ test_scipts/slowReadA.sh
@@ -3,4 +3,5 @@ export proxy15440=127.0.0.1
 export proxyport15440=33345
 export pin15440=123123123
 
-LD_PRELOAD=./lib/lib440lib.so ./basiclru
+gcc BasicLRU.c -o basiclru
+LD_PRELOAD=../lib/lib440lib.so ./basiclru
